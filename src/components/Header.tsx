ad40fa30fd6eb5a6497820bea316ce77e86e7510
skaur1604 <skaur1604@academic.rrc.ca>
2026-01-26 22:44:07 -0600

    Fixed logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,11 @@
 export function Header() {
   return (
     <header>
+      <img
+        src="/logo.svg"
+        alt="Pixell River Financial Logo"
+        style={{ height: "60px" }}
+      />
       <h1>Pixell River Employee Directory</h1>
       <p>Internal staff listing for company use</p>
     </header>
